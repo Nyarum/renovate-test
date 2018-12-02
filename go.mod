91e83014874aa5a9172888db5908c23edbd0d42b
Nyarum <nyarumilian@gmail.com>
2018-12-02 23:19:56 +0400

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Nyarum/renovate-test
 
 go 1.12
 
-require rsc.io/quote v1.5.2
+require (
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c
+	rsc.io/quote v1.5.2
+)
--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 
+	"golang.org/x/text/language"
+
+	"golang.org/x/text/currency"
+
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println(quote.Hello())
+	fmt.Println(currency.FromTag(language.Amharic))
+
 }
